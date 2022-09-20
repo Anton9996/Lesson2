21e95c43e536a9ff0c57f5ecce90b6d2fa9329a5
Anton <korjant09996@gmail.com>
2022-09-20 18:24:09 +0400

    complite 15

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели и проверяет, является ли это выходным
+Console.WriteLine($"Введите число, от одного до семи, я скажу, выходной это в разрезе недели или нет");
+Console.WriteLine($"Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int Weekend(int num)
+{
+ return num  == 6 || num  == 7 ? 1 : 0;
+}
+if (0 < a && a < 8) {
+int result = Weekend(a);
+string res = result == 1 ? "выходной" : "будни";
+Console.Write($"{a} -> {res}");;
+}
+else Console.Write($"{a}-> в недели 7 дней");
