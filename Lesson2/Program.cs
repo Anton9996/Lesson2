b44fd4b2c6d3f665b2d7bc1167f587c24fb740af
Anton <korjant09996@gmail.com>
2022-09-20 18:04:37 +0400

    complite 1

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимаеи на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.WriteLine($"Введите трехзначное число, и я выведу 2 цифру этого числа.Зачем? Да просто! ");
+Console.WriteLine($"Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+int SecondNumber(int num)
+{
+    int number2 = ((num/10)%10);
+    return number2;
+}
+if (99 < a && a < 1000) {
+int result = SecondNumber(a);
+Console.Write($"{a} -> {result}");
+}
+else Console.Write($"Это не трехзначное число");
